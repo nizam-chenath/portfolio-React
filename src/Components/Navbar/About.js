cf854556055fe5b4b4c0e24b16ffc04fe72fb4e6
nizam <nizamstudymajliz@gmail.com>
2021-08-28 23:32:56 +0530

    anime Home

--- src/Components/Navbar/About.js
+++ src/Components/Navbar/About.js
@@ -4,7 +4,14 @@ import "./About.css";
 function About() {
     return (
         <div className="about">
+            <div>
             <p className="dot">.</p>
+            <h2 className="heading">RESUME</h2>
+            <h2 className="subHeading">Experience</h2>
+            <div className="box"></div>
+            <div className="box2"></div>
+            </div>
+            
         </div>
     )
 }
--- src/Components/Navbar/Home.js
+++ src/Components/Navbar/Home.js
@@ -6,7 +6,7 @@ function Home() {
         <div className="home">
             <div className="heading">
             <h2 className="heading-setuph2">Hello..It's me, </h2>
-            <h1 className="heading-setuph1">Nizam</h1>
+            <h1 className="heading-setuph1" data-text="Nizam_chenath">Nizam</h1>
             <p className="para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex eveniet laboriosam in
                  odit praesentium architecto ullam atque, cum
                  assumenda laborum exercitationem modi, libero id!.hahah ih
